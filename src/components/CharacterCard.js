e22fdb5dd82bc62cfb9b63b38d9270224bff55b7
marvin lewis <marvinlewis01@gmail.com>
2020-02-28 09:21:14 -0800

    characters showing, imported and used reactstrap to create character card, added css

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
+import "./card.css";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+export default function CharacterCard({character}) {
+  console.log(character);
+    return (
+        <Card className="cont">
+          <div  className="pic">
+          <CardImg top width="100%" src={character.image} alt={character.name} />
+          </div>
+          <CardBody>
+          <CardTitle>{character.name}</CardTitle>
+           <CardText>{character.name} is from planet {character.location.name}. {character.name} gender is {character.gender}</CardText>
+            <CardText>
+            </CardText>
+          </CardBody>
+        </Card>
+  )
+};
+
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+    const [ characters, setCharacters ] = useState([]);
 
   useEffect(() => {
+    axios.get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
+    .then(res => {
+      setCharacters(res.data.results);
+    })
+    .catch(res => console.log("error"))
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+    {characters.map(item => { return(
+      <CharacterCard key={item.id} character={item} />)
+    })}
+    </div>
   );
 }
